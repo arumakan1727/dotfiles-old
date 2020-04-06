6fca874da8b5416937ad1c07494d0727b06c7ef4
arumakan1727 <aluminumcan7271@gmail.com>
2020-04-06 14:58:35 +0900

    [add] .config/以下のファイルをいくつか追加

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+#=============================
+# .config/fish/config.fish
+#=============================
+
+#aliases
+alias cp "cp -i"
+alias mv "mv -i"
+alias rm "rm -i"
+alias thistory "history --show-time='%Y-%m-%d %H:%M:%S '"
+
+source $HOME/.profile
